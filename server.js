3298e70467ec6c5c20c0e08942cb1847619bd6ea
Oscarzhangtc <you@example.com>
2019-11-02 15:38:19 -0400

    changes

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const app = express();
 
 app.use(express.json());
 
-app.get('*', (req, res)=>{
-    res.send("<h1>if you see this message, the server is working.</h1>")
+app.get('/', (req, res)=>{
+    res.send("if you see this message, the server is working")
 
 })
-app.post('*', (req, res)=>{
+app.post('/', (req, res)=>{
     console.log(req.body)
-
 })
 app.listen(3000, ()=>{
     console.log("the express server is listening of port 3000"); 
