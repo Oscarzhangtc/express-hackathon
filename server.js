556ba33ca20f3a962df45c9c12e4f85a97564bef
Oscarzhangtc <you@example.com>
2019-11-02 15:07:13 -0400

    get mehod

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const app = express();
 
 app.use(express.json());
 
+app.get('*', (req, res)=>{
+    res.send("<h1>if you see this message, the server is working.</h1>")
+
+})
 app.post('*', (req, res)=>{
     console.log(req.body)
+
 })
 app.listen(3000, ()=>{
     console.log("the express server is listening of port 3000"); 
